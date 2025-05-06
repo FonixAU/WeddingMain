6f49814efd5e0ea86203a59cda8ca5a9db75f87f
Felipe <54015661+FonixAU@users.noreply.github.com>
2025-05-06 08:24:52 +0000

    Green?

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -53,7 +53,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         {/* This makes the page responsive */}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           {/* Whole Non-image card style, the height was adjusted manually to set the max viewport. */}
-          <div className="after:content relative mb-5 flex h-[400px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-[#895129]/10 px-6 pb-16 pt-64 text-center text-black shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+          <div className="after:content relative mb-5 flex h-[400px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-[#96aa81]/40 px-6 pb-16 pt-64 text-center text-black shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
               Grantiz Wedding Photos
             </h1>
