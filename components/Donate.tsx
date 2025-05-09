f3a9e6e24fe6970f692d64b89e48bf7538cc763a
Felipe <54015661+FonixAU@users.noreply.github.com>
2025-05-09 10:38:43 +0000

    finish most of registry

--- components/Donate.tsx
+++ components/Donate.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Image from "next/image";
 //Icons
 import Exit from "./Icons/Exit";
-import PayPal from "./Icons/PayPal";
-
 export default function Donate() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,32 +12,24 @@ export default function Donate() {
         className="button is-large secondary clabel">
         Donate here!
       </button>
-
       {isOpen && (
        <div className="fixed inset-0 flex items-center justify-center bg-[#F9F6EE] bg-opacity-50 z-50">
-       <div className="relative w-auto bg-black rounded-lg overflow-hidden shadow-lg flex flex-col p-2">
-         <div className="text-lg font-bold text-indigo-500 mb-4">
+       <div className="relative w-auto bg-[#F9F6EE] rounded-lg overflow-hidden shadow-lg flex flex-col p-2">
+         <div className="font-bold title clabel mb-4">
           Options
          </div>
-         <div className="flex flex-col border-2 rounded-xl border-indigo-500 
-         flex items-center justify-center text-black mb-3 w-fit mx-auto p-2
-          text-base font-semibold tracking-tight">
-         <span className="flex inline-block text-indigo-500">
+         <div className="flex flex-col items-center justify-center subtitle mb-3 w-fit mx-auto p-2
+          font-bold tracking-tight">
+         <span className="flex inline-block">
             Acc Name: Honeymoon
         </span>
-        <span className="flex inline-block text-indigo-500">
-            BSB:
+        <span className="flex inline-block">
+            BSB: 659000
         </span>
-        <span className="flex inline-block text-indigo-500">
-          Acc No:
+        <span className="flex inline-block">
+          Acc No: 775117S37
         </span>
         </div>
-        
-         {/* PayPal Logo Centeblack */}
-         <div className="flex self-center size-11">
-           <PayPal />
-         </div>
-
          {/* QR Code at the Bottom */}
          <div className="mt-auto flex justify-center">
            <Image
